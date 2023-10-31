b9acefad58838cc7e1e37d7720de12736df61c7c
mzf11125 <mzidanfatonie@gmail.com>
2023-10-31 21:15:51 +0700

    update

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css'; // Import your CSS file for TodoList
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function TodoList({ todos, onDelete }) {
   return (
     <ul className="todo-list">
       {todos.map((todo) => (
         <li key={todo.id} className="todo-item">
-          <span>{todo.title}</span> {/* Assuming 'title' is the property for todo text */}
-          <button onClick={() => onDelete(todo.id)}>Delete</button>
-        </li>
+        <span>{todo.title}</span>
+        <button onClick={() => onDelete(todo.id)}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </li>
       ))}
     </ul>
   );
